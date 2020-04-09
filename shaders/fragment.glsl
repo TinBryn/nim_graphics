1e0be9881c86f658aebf827c920325be59f316d6
Kieran Griffiths <griffk04@gmail.com>
2020-04-09 11:27:06 +0800

    Updated the shader to use show use of varyings and uniforms
     - The colour shift function works by transforming the RGB colour
       to YIQ and rotating the IQ portion and then back to RGB.
       This could also be a conversion to HSL, shift H and then back
       to RGB.

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,8 +1,32 @@
 #version 330 core
 
+uniform float shift;
+
+in vec3 vCol;
+
 out vec4 FragColor;
+
+vec3 shiftHue(vec3 input, float shift)
+{
+  float u = cos(shift);
+  float w = sin(shift);
+
+  vec3 ret;
+  ret.r = (0.299 + 0.701*u + 0.168*w) * input.r
+        + (0.587 - 0.587*u + 0.330*w) * input.g
+        + (0.114 - 0.114*u - 0.497*w) * input.b;
+  
+  ret.g = (0.299 - 0.299*u - 0.328*w) * input.r
+        + (0.587 + 0.413*u + 0.035*w) * input.g
+        + (0.114 - 0.114*u + 0.292*w) * input.b;
+  
+  ret.b = (0.299 - 0.300*u + 1.250*w) * input.r
+        + (0.587 - 0.580*u - 1.050*w) * input.g
+        + (0.114 + 0.886*u - 0.203*w) * input.b;
+  return ret;
+}
 
 void main()
 {
-  FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+  FragColor = vec4(shiftHue(vCol, shift), 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 
 in vec3 aPos;
+in vec3 aCol;
+
+out vec3 vCol;
 
 void main()
 {
+  vCol = aCol;
   gl_Position = vec4(aPos, 1.0);
 }
