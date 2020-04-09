c7e0e1ebc6a5bbe8c1c4607dcb23e8a96224b779
Kieran Griffiths <griffk04@gmail.com>
2020-04-09 11:30:08 +0800

    Added a resize callback
     - Changes the glViewport to reflect the new size.

--- src/callbacks.nim
+++ src/callbacks.nim
@@ -1,7 +1,12 @@
-import nimgl/glfw
+import nimgl/[glfw, opengl]
 
 proc setupCallbacks*(window: GLFWWindow) =
   proc keyboard(window: GLFWwindow, key, code, action, mods: int32) {.cdecl.} =
     if glfw.toGLFWKey(key) == GLFWKey.ESCAPE:
       window.setWindowShouldClose(true)
+
+  proc resize(window: GLFWwindow, width, height: int32) {.cdecl.} =
+    glViewPort(0, 0, width, height)
+
   discard setKeyCallback(window, keyboard)
+  discard setWindowSizeCallback(window, resize)
