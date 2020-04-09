d3b58bdec183ce123f71b7e76d63e1b1211a3298
Kieran Griffiths <griffk04@gmail.com>
2020-04-09 12:08:42 +0800

    Changed to an animated render
     - Now renders a square (or rectangle depending on aspect ratio)
       with multiple interpolated colours. The colours also shift via
       sending a uniform to the shader before rendering.
    
     - The openGL context is now explicitly 3.3 core.

--- src/graphics.nim
+++ src/graphics.nim
@@ -1,16 +1,54 @@
-import nimgl/[glfw,opengl], glm, shaders, glsugar, callbacks
+import nimgl/[glfw,opengl], glm, shaders, glsugar, callbacks, constants, math
 
 proc main =
-  var triangle = [
+  # let triangle = [
+  #   vec3f(-0.5, -0.5, 0.0),
+  #   vec3f(0.5, -0.5, 0.0),
+  #   vec3f(0.0, 0.5, 0.0)
+  # ]
+
+  type Colours = enum
+    orange,
+    green,
+    blue,
+    pink,
+
+  var res: array[Colours, Vec3f]
+  res[orange] = vec3f(0.8, 0.4, 0.1)
+  res[green] = vec3f(0.1, 0.8, 0.1)
+  res[blue] = vec3f(0.1, 0.4, 0.8)
+  res[pink] = vec3f(0.8, 0.1, 0.8)
+
+  let pallette = res
+
+  let square = [[
     vec3f(-0.5, -0.5, 0.0),
     vec3f(0.5, -0.5, 0.0),
-    vec3f(0.0, 0.5, 0.0)
-  ]
+    vec3f(0.5, 0.5, 0.0),
+    vec3f(-0.5, 0.5, 0.0),
+    vec3f(-0.5, -0.5, 0.0),
+    vec3f(0.5, 0.5, 0.0)
+  ],[
+    pallette[blue],
+    pallette[green],
+    pallette[orange],
+    pallette[pink],
+    pallette[blue],
+    pallette[orange]
+  ]]
+
+  var shape = square[0]
+  var colours = square[1]
 
   if not glfwInit():
     quit("GLFW initialisation failed")
 
+  glfwWindowHint(GLFWContextVersionMajor, 3)
+  glfwWindowHint(GLFWContextVersionMinor, 3)
+  glfwWindowHint(GLFWOpenglProfile, GLFWOpenglCoreProfile)
+
   let window = glfwCreateWindow(800, 600, "Window", nil, nil, false)
+  glfwSwapInterval(1)
   setupCallbacks(window)
   window.makeContextCurrent()
   assert glInit()
@@ -19,12 +57,13 @@ proc main =
   var shader = createShader(vertexSource, fragmentSource)
 
   let location_aPos = shader["aPos"]
+  let location_aCol = shader["aCol"]
 
   shader.use()
 
   const
     nVAOs = 1
-    nVBOs = 1
+    nVBOs = 2
 
   var
     vao: array[nVAOs, uint32]
@@ -34,17 +73,30 @@ proc main =
 
   glBindVertexArray(vao[0])
   glBindBuffer(GL_ARRAY_BUFFER, vbo[0])
-  glBufferData(GL_ARRAY_BUFFER, sizeof triangle, addr triangle[0], GL_STATIC_DRAW)
+  glBufferData(GL_ARRAY_BUFFER, sizeof shape, addr shape[0], GL_STATIC_DRAW)
 
   vertexAttribPointer(location_aPos, 3, GLfloat, false, 3 * sizeof(GLfloat), 0)
   glEnableVertexAttribArray(location_aPos)
 
-  glPolygonMode(GLFrontAndBack, GLLine)
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[1])
+  glBufferData(GL_ARRAY_BUFFER, sizeof colours, addr colours[0], GL_STATIC_DRAW)
+
+  vertexAttribPointer(location_aCol, 3, GLfloat, false, 3 * sizeof(GLfloat), 0)
+  glEnableVertexAttribArray(location_aCol)
+
+  glPolygonMode(GLFrontAndBack, GLFill)
+
+  var shift = 0f
+  ## the old colour I was using `vec3f(1.0, 0.5, 0.2)`
 
   while not window.windowShouldClose():
     glfwPollEvents()
     glClear(GL_COLOR_BUFFER_BIT)
-    glDrawArrays(GLTriangles, 0, triangle.len.GLsizei)
+    shader{"shift"} = shift
+    shift += 1/32f
+    if shift >= 2*PI:
+      shift -= 2*PI
+    glDrawArrays(GLTriangles, 0, shape.len.GLsizei)
     window.swapBuffers()
 
 when isMainModule:
