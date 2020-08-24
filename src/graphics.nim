72523d164f39ba07c4f6a9e4dc53d87bd08735f1
Kieran <griffk04@gmail.com>
2020-08-24 12:50:20 +0800

    Refactored some constant data to be constant

--- src/graphics.nim
+++ src/graphics.nim
@@ -1,27 +1,20 @@
 import nimgl/[glfw,opengl], glm, shaders, glsugar, callbacks, constants, math
 
 proc main =
-  # let triangle = [
-  #   vec3f(-0.5, -0.5, 0.0),
-  #   vec3f(0.5, -0.5, 0.0),
-  #   vec3f(0.0, 0.5, 0.0)
-  # ]
-
   type Colours = enum
     orange,
     green,
     blue,
     pink,
 
-  var res: array[Colours, Vec3f]
-  res[orange] = vec3f(0.8, 0.4, 0.1)
-  res[green] = vec3f(0.1, 0.8, 0.1)
-  res[blue] = vec3f(0.1, 0.4, 0.8)
-  res[pink] = vec3f(0.8, 0.1, 0.8)
+  const pallette: array[Colours, Vec3f] = [
+    vec3f(0.8, 0.4, 0.1),
+    vec3f(0.1, 0.8, 0.1),
+    vec3f(0.1, 0.4, 0.8),
+    vec3f(0.8, 0.1, 0.8)
+  ]
 
-  let pallette = res
-
-  let square = [[
+  const square = [[
     vec3f(-0.5, -0.5, 0.0),
     vec3f(0.5, -0.5, 0.0),
     vec3f(0.5, 0.5, 0.0),
